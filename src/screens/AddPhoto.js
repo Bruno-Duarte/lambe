dc680a335cbcd4d5334ff707016334f0d77bc0a0
bruno <bruno@sementedigital.com.br>
2021-08-04 23:43:16 -0300

    End of class 204

--- src/screens/AddPhoto.js
+++ src/screens/AddPhoto.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {
     View,
     Text,
@@ -6,13 +7,14 @@ import {
     TouchableOpacity,
     TextInput,
     Image,
-    Dimentions,
+    Dimensions,
     Platform,
     ScrollView,
     Alert,
-    Dimensions
 } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
+
+import {addPost} from '../store/actions/posts';
 
 class AddPhoto extends Component {
     state = {
@@ -38,7 +40,19 @@ class AddPhoto extends Component {
     };
 
     save = async () => {
-        Alert.alert('Imagem adicionada!', this.state.comment);
+        this.props.onAddPost({
+            id: Math.random(),
+            nickname: this.props.name,
+            email: this.props.email,
+            image: this.state.image,
+            comments: [{
+                nickname: this.props.name,
+                comment: this.state.comment,
+            }]
+        });
+
+        this.setState({image: null, comment: ''});
+        this.props.navigation.navigate('Feed');
     };
 
     render() {
@@ -116,4 +130,19 @@ const styles = StyleSheet.create({
     },
 });
 
-export default AddPhoto;
+const mapStateToProps = ({user}) => {
+    return {
+        email: user.email,
+        name: user.name,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPost: post => dispatch(addPost(post))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPhoto);
+
+// export default AddPhoto;
--- src/screens/Feed.js
+++ src/screens/Feed.js
@@ -1,44 +1,17 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {StyleSheet, FlatList, View} from 'react-native';
 
 import Header from '../components/Header';
 import Post from '../components/Post';
 
 class Feed extends Component {
-    state = {
-        posts: [
-            {
-                id: Math.random(),
-                nickname: 'Rafael Pereira Filha',
-                email: 'rafaeleprrfh@gmail.com',
-                image: require('../../assets/imgs/fence.jpg'),
-                comments: [
-                    {
-                        nickname: 'John Ray Sheldon',
-                        comment: 'Stunning!',
-                    },
-                    {
-                        nickname: 'Ana Julia Arruda',
-                        comment: 'Foto linda! Onde foi tirada?'
-                    }
-                ]
-            },
-            {
-                id: Math.random(),
-                nickname: 'Francisco Leandro Lima',
-                email: 'fllima@gmail.com',
-                image: require('../../assets/imgs/bw.jpg'),
-                comments: []
-            },
-        ]
-    }
-
     render() {
         return (
             <View style={styles.container}>
                 <Header />
                 <FlatList
-                    data={this.state.posts}
+                    data={this.props.posts}
                     keyExtractor={item => `${item.id}`}
                     renderItem={({item}) => 
                         <Post key={item.id} {...item} />
@@ -58,4 +31,12 @@ const styles = StyleSheet.create({
     }
 });
 
-export default Feed;
+const mapStateToProps = ({posts}) => {
+    return {
+        posts: posts.posts
+    };
+};
+
+export default connect(mapStateToProps)(Feed);
+
+// export default Feed;
