bb278669a6e9fd63b9567dfd8aab417e6ece7364
bruno <bruno@sementedigital.com.br>
2021-08-06 23:21:45 -0300

    End of class 210

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {AppRegistry} from 'react-native';
+
+import axios from 'axios';
+axios.defaults.baseURL = 
+    'https://lambe-28c98-default-rtdb.firebaseio.com/';
+
 import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
-
 import storeConfig from './src/store/storeConfig';
+
 
 const store = storeConfig();
 const Redux = () => (
--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,4 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+import {
+    createStore, 
+    combineReducers,
+    compose,
+    applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
+
 import userReducer from './reducers/user';
 import postsReducer from './reducers/posts';
 
@@ -8,7 +15,10 @@ const reducers = combineReducers({
 });
 
 const storeConfig = () => {
-    return createStore(reducers);
-}
+    return createStore(
+        reducers, 
+        compose(applyMiddleware(thunk))
+    );
+};
 
 export default storeConfig;
